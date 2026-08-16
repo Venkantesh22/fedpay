37ff6291babac302c29262abaaf53c92aefdb052
Venkatesh22 <venkateshrathod242002@gmail.com>
2026-08-16 17:27:11 +0530

    add a prepaid card  in application

--- lib/views/screens/dashboard/dashboard_screen.dart
+++ lib/views/screens/dashboard/dashboard_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:lekra/services/date_formatters_and_converters.dart';
-import 'package:lekra/views/screens/dashboard/card/form_for_apply_card/form_for_apply_card_screen.dart';
+import 'package:lekra/views/screens/dashboard/card/card_screen/main_card_screen/main_card_screen.dart';
 import 'package:lekra/views/screens/dashboard/home_screen/home_screen.dart';
 import 'package:lekra/views/screens/transcation_history/transaction_history_screen.dart';
 import 'package:lekra/views/screens/drawer_screen/drawer_screen.dart';
@@ -50,7 +50,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                     isReload: true,
                     scaffoldKey: _dashboardScaffoldKey,
                   ),
-                  // FormForApplyCardScreen(),
+                  MainCardScreen(),
                   TransactionHistoryScreen(
                     fromDateValue: DateTime(2024, 1, 1),
                     todateValue: getDateTime(),
@@ -92,17 +92,17 @@ class _DashboardScreenState extends State<DashboardScreen> {
                           icon: Assets.svgsHome,
                           isActive: controller.dashPage == 0,
                         ),
-                        // BottomNavigationItemWidget(
-                        //   onTap: () => controller.dashPage = 1,
-                        //   title: 'Card',
-                        //   icon: Assets.svgsCard,
-                        //   isActive: controller.dashPage == 1,
-                        // ),
                         BottomNavigationItemWidget(
                           onTap: () => controller.dashPage = 1,
+                          title: 'Card',
+                          icon: Assets.svgsCard,
+                          isActive: controller.dashPage == 1,
+                        ),
+                        BottomNavigationItemWidget(
+                          onTap: () => controller.dashPage = 2,
                           title: 'Report',
                           icon: Assets.svgsReport,
-                          isActive: controller.dashPage == 1,
+                          isActive: controller.dashPage == 2,
                         ),
                       ],
                     ),
